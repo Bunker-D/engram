a32196feb73e1295e76eb50d21071f43299734ad
Bunker D <contact@bunkerd.fr>
2025-02-13 01:19:46 -0800

    KB Gui can show key and pair values (with click (de)selection)

--- src/engram/gui/keyboard.py
+++ src/engram/gui/keyboard.py
@@ -1,63 +1,11 @@
 from math import ceil, cos, radians, sin
-from typing import Literal, NoReturn
+from typing import Callable
 
 from PySide6.QtGui import QTransform
 from PySide6.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QWidget
 
 from engram.gui.filled_button import FillMode, W_FilledButton  # TODO  Relative import
-
-
-class Key:  # HACK Key class shouldn't be here
-    hand: Literal["L", "R"]
-    finger: Literal[0, 1, 2, 3, 4]
-    h_dist: int  # h_dist > 0 ⇔ inward
-    v_dist: int  # v_dist > 0 ⇔ upward
-
-    # TEST: Key.__init__(…)
-    def __init__(self, finger: str, h_dist: int, v_dist: int) -> None:
-        finger = finger.upper()
-        try:
-            self.hand = finger[0]  # type: ignore
-            assert self.hand in "LR"
-            self.finger = "T1234".index(finger[1])  # type: ignore
-        except (ValueError, AssertionError):
-            self.__raise_invalid_finger(finger)
-        self.h_dist = h_dist
-        self.v_dist = v_dist
-
-    @staticmethod
-    def __raise_invalid_finger(finger: str) -> NoReturn:
-        raise ValueError(f"Invalid finger: {finger}")
-
-    # TEST: Key.__str__()
-    def __str__(self) -> str:
-        return f"{self.hand}{self.finger or 'T'}{self.__movement()}"
-
-    def __movement(self) -> str:
-        movement = ""
-        y = self.v_dist
-        x = self.h_dist
-        if self.hand == "R":
-            x *= -1
-        while y or x:
-            x_sign = self.__sign(x)
-            y_sign = self.__sign(y)
-            movement += {
-                (-1, +1): "↖", (0, +1): "↑", (+1, +1): "↗",
-                (-1,  0): "←", (0,  0): "●", (+1,  0): "→",
-                (-1, -1): "↙", (0, -1): "↓", (+1, -1): "↘",
-            }[x_sign, y_sign]  # fmt: skip
-            x -= x_sign
-            y -= y_sign
-        return movement or "●"
-
-    @staticmethod
-    def __sign(x: int) -> Literal[-1, 0, 1]:
-        if x < 0:
-            return -1
-        if x > 0:
-            return 1
-        return 0
+from engram.scoring import Key
 
 
 class W_Key(W_FilledButton):
@@ -123,12 +71,19 @@ class W_Key(W_FilledButton):
 
 class W_Keyboard(QGraphicsView):
     keys: dict[Key, W_Key]
+    __key_values: Callable[[Key], float] | None
+    __pair_values: Callable[[Key, Key], float] | None
+    __active_key: Key | None
 
     def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
         self.keys = {}
         self.__build_sofle()  # HACK Keyboard directly implemented as Sofle
+        self.__set_click_actions()
         self.__compute_geometry()
+        self.__key_values = None  # TODO Initial value functions?
+        self.__pair_values = None
+        self.__active_key = None
 
     def __build_sofle(self) -> None:
         scene = QGraphicsScene(self)
@@ -170,15 +125,69 @@ class W_Keyboard(QGraphicsView):
         bounding_rect.adjust(-margin, -margin, margin, margin)
         self.setFixedSize(ceil(bounding_rect.width()), ceil(bounding_rect.height()))
 
+    def __set_click_actions(self) -> None:
+        for key, key_btn in self.keys.items():
+            key_btn.clicked.connect(lambda _, key=key: self.__toggle_key(key))
+
+    def __toggle_key(self, key: Key) -> None:
+        was_active = key is self.__active_key
+        self.__deactivate_key()
+        if not was_active:
+            self.__activate_key(key)
+        self.update_shown_values()
+
+    def __deactivate_key(self) -> None:
+        if not self.__active_key:
+            return
+        self.keys[self.__active_key].setChecked(False)
+        self.__active_key = None
+
+    def __activate_key(self, key: Key) -> None:
+        self.keys[key].setChecked(True)
+        self.__active_key = key
+
+    def set_values(
+        self,
+        for_keys: Callable[[Key], float],
+        for_pairs: Callable[[Key, Key], float] | None = None,
+    ) -> None:
+        self.__key_values = for_keys
+        self.__pair_values = for_pairs
+        self.update_shown_values()
+
+    def update_shown_values(self) -> None:
+        val_fun: Callable[[Key], float]
+        if self.__active_key:
+            if self.__pair_values:
+                val_fun = lambda key: self.__pair_values(self.__active_key, key)  # type:ignore
+            else:
+                val_fun = lambda _: 0
+        else:
+            val_fun = self.__key_values or (lambda _: 0)
+        for key, key_btn in self.keys.items():
+            key_btn.fill = val_fun(key)
+            key_btn.update()
+
 
 if __name__ == "__main__":
     import sys
+    from math import sqrt
 
     from engram.gui.esc_window import EscapableWindow
 
     app = QApplication(sys.argv)
+
+    keyboard = W_Keyboard()
+    keyboard.set_values(
+        lambda k: (1 + abs(k.dx) + abs(k.dy)) / 5,
+        lambda k, h: sqrt((k.finger - k.dx - h.finger + h.dx) ** 2 + (k.dy - h.dy) ** 2)
+        / 7
+        if k.hand == h.hand
+        else 0,
+    )
+
     window = EscapableWindow()
-    window.setCentralWidget(W_Keyboard())
+    window.setCentralWidget(keyboard)
 
     window.show()
     sys.exit(app.exec())
